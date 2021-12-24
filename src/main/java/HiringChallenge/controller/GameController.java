33915b9d24412851b64486dcaa7f47fa60f90a5b
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2021-12-24 16:08:28 +0530

    Initial Commit

---
+++ src/main/java/HiringChallenge/controller/GameController.java
@@ -0,0 +1,86 @@
+package HiringChallenge.controller;
+
+import HiringChallenge.model.PlayModel;
+import HiringChallenge.model.StartModel;
+import HiringChallenge.repository.GameDao;
+import HiringChallenge.repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import HiringChallenge.utils.Util;
+
+@RestController
+@RequestMapping("/")
+public class GameController {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    Util util = new Util();
+
+    @GetMapping("/start")
+    public StartModel startGame() {
+        String token = util.generateToken();
+        System.out.println("Token : "+token);
+        GameDao newGame = new GameDao();
+        newGame.setToken(token);
+        newGame.setServerScore(0);
+        newGame.setUserScore(0);
+        System.out.println("Game : "+newGame);
+        gameRepository.save(newGame);
+        StartModel model = StartModel.build(token,"READY");
+
+        return model;
+    }
+
+    @GetMapping("/v1/{token}/{userMove}")
+    public PlayModel playGameRandom(@PathVariable("token") String token, @PathVariable("userMove") String userMove) {
+        GameDao game = gameRepository.findById(token).get();
+        System.out.println("Game : "+game);
+
+        String serverMove = util.generateRandomServerMove();
+        System.out.println("ServerMove : "+serverMove);
+        System.out.println("UserMove : "+userMove);
+        int[] score = util.findWinner(userMove,serverMove);
+        System.out.println("Score[0] : "+score[0]);
+        System.out.println("Score[1] : "+score[1]);
+
+        game.setUserScore(game.getUserScore()+score[0]);
+        game.setServerScore(game.getServerScore()+score[1]);
+
+        System.out.println("Game After : "+game);
+        gameRepository.save(game);
+
+        if(game.getUserScore()>=3 || game.getServerScore()>=3)
+            gameRepository.deleteById(token);
+
+        return PlayModel.build(serverMove,game.getServerScore()+game.getUserScore());
+    }
+
+
+    @GetMapping("/v2/{token}/{userMove}")
+    public PlayModel playGameStrategic(@PathVariable("token") String token, @PathVariable("userMove") String userMove) {
+        GameDao game = gameRepository.findById(token).get();
+        System.out.println("Game : "+game);
+
+        String serverMove = util.generateStrategicServerMove(userMove);
+        System.out.println("ServerMove : "+serverMove);
+        System.out.println("UserMove : "+userMove);
+        int[] score = util.findWinner(userMove,serverMove);
+        System.out.println("Score[0] : "+score[0]);
+        System.out.println("Score[1] : "+score[1]);
+
+        game.setUserScore(game.getUserScore()+score[0]);
+        game.setServerScore(game.getServerScore()+score[1]);
+
+        System.out.println("Game After : "+game);
+        gameRepository.save(game);
+
+        if(game.getUserScore()>=3 || game.getServerScore()>=3)
+            gameRepository.deleteById(token);
+
+        return PlayModel.build(serverMove,game.getServerScore()+game.getUserScore());
+    }
+}
