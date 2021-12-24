b3ead503a52516ce946a270f7763b8e53736daaa
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2021-12-24 16:05:03 +0530

    Initial Commit

---
+++ src/main/java/HiringChallenge/HiringChallengeApplication.java
@@ -0,0 +1,15 @@
+package HiringChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class HiringChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringChallengeApplication.class, args);
+	}
+
+}
